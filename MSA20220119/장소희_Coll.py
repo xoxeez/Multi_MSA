d34cbfcb9c014bd63bebf20391b419b135552ed5
SONY <52715471+xoxeez@users.noreply.github.com>
2022-01-19 17:33:11 +0900

    Update 장소희_Coll.py

--- MSA20220119/장소희_Coll.py
+++ MSA20220119/장소희_Coll.py
@@ -4,7 +4,7 @@ scoreSum = dicScore["국어"] + dicScore["영어"] + dicScore["수학"] + dicSco
 print("1. 평균 점수 : {0}".format(scoreSum / len(dicScore)))
 print("-" * 10)
 
-# 2. set을 이용해서 1~100까지 숫자 중에 3과 5의 공배수와 합집합을 구함
+# 2. set을 이용해서 1~100까지 숫자 중에 3과 5의 공배수를 구함
 multi3 = {i for i in range(1, 101) if i % 3 == 0}
 multi5 = {i for i in range(1, 101) if i % 5 == 0}
 print("2. 3과 5의 공배수 : {0}".format(multi3 & multi5))
@@ -12,7 +12,7 @@ print("2. 3과 5의 합집합 : {0}".format(multi3 | multi5))
 print("-" * 10)
 
 # 3. 리스트 데이터 : 7, 5, 3, 1, -1, -3, -5, -7 출력 (range 활용)
-oddList = list(range(7, -7, -2))
+oddList = list(range(7, -8, -2))
 print("3. 리스트 출력 : {0}".format(oddList))
 print("-" * 10)
 
