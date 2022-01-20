056f7557aeca1359a72569ac08ebb1c840a961a2
SONY <52715471+xoxeez@users.noreply.github.com>
2022-01-20 17:26:40 +0900

    Create 장소희_Ext1.py

---
+++ MSA20220120/장소희_Ext1.py
@@ -0,0 +1,14 @@
+# 1. 각 학생의 합계와 평균 + 전체 합계와 평균
+# for문
+
+score = [[95, 90, 80, 50], [100, 50, 90, 90], [99, 60, 100, 40], [55, 80, 80, 60]]
+scoreSum = []
+
+for i in range(len(score)):
+    scoreSum.append(sum(score[i]))
+scoreAvg = [scoreSum[i]/len(score) for i in range(len(score))]
+
+# 출력하기
+for i in range(len(score)):
+    print("학생{0}의 합계 : {1}, 평균 : {2}".format(i + 1, scoreSum[i], scoreAvg[i]))
+print("전체학생의 합계 : {0}, 평균 : {1}".format(sum(scoreSum), sum(scoreAvg)/len(score)))
